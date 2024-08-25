cad81c2e3e9f6720b5a08ad24a570020ee461889
btntazo <tamazimirianashvili@gmail.com>
2024-08-25 21:16:04 +0400

    new changes

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿
+using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand: ICommand<Unit> //empty icommand that does not return any response
+    {
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse> //icommand that returns a generic response
+    {
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, List<string> Category,  string ImageFile, decimal Price): ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create product from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Description = command.Description,
+                Category = command.Category,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //save product to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //return CreateProductResult
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
